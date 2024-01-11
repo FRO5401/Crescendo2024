4526700eec585143b16b3a36d29e62df9deb53ff
Jarrard Santos <108013218+App1eCid3r@users.noreply.github.com>
2024-01-10 20:11:17 -0500

    30 min check

--- Crescendo2024/src/main/java/frc/robot/Subsystems/Drivebase.java
+++ Crescendo2024/src/main/java/frc/robot/Subsystems/Drivebase.java
@@ -2,7 +2,57 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.Solenoid;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.SPI;
+import java.util.Collection;
 
 /** Add your docs here. */
-public class Drivebase {}
+public class Drivebase {
+    private CANSparkMax leftDrive1;
+    private CANSparkMax leftDrive2;
+    private CANSparkMax leftDrive3;
+  
+    private CANSparkMax rightDrive1;
+    private CANSparkMax rightDrive2;
+    private CANSparkMax rightDrive3;
+  
+    private SparkMaxPIDController leftDrive1PidController;
+    private SparkMaxPIDController leftDrive2PidController;
+    private SparkMaxPIDController leftDrive3PidController;
+  
+    private SparkMaxPIDController rightDrive1PidController;
+    private SparkMaxPIDController rightDrive2PidController;
+    private SparkMaxPIDController rightDrive3PidController;
+  
+    DigitalInput minSwitch, maxSwitch;
+    
+
+    private MotorControllerGroup leftDrives;
+  private MotorControllerGroup rightDrives;
+
+  private DifferentialDrive allDrive;
+
+  private DifferentialDriveOdometry odometry;
+
+  private int motorCurrent = 24;
+
+  private boolean compressorState = false;
+ }
