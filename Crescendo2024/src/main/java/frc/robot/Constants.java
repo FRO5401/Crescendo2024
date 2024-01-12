7bf305332fe09116fd63869ffed08b1b8fcd5bd8
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2024-01-12 18:28:20 -0500

    finished drivebase
    added xbox move anf controls

---
+++ Crescendo2024/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public final class Constants {
+    public static class DriveMotorConstants{
+public static final int LEFT_DRIVE1_ID = 1;
+public static final int LEFT_DRIVE2_ID = 2;
+public static final int RIGHT_DRIVE1_ID = 3;
+public static final int RIGHT_DRIVE2_ID = 4;
+
+    }
+    public static class ControlConstants{
+public static final int XBOX_CONTROLLER_DRIVER = 0;
+public static final int XBOX_CONTROLLER_OPERATOR =1;
+public static final double AXIS_THRESHOLD = 0.05;
+public static final double SPIN_SENSITIVITY = 0.8;
+
+}
+}
+
--- Crescendo2024/src/main/java/frc/robot/Subsystems/Drivebase.java
+++ Crescendo2024/src/main/java/frc/robot/Subsystems/Drivebase.java
@@ -4,14 +4,70 @@
 
 package frc.robot.Subsystems;
 
+//Imports 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Drivebase extends SubsystemBase {
   /** Creates a new Drivebase. */
-  public Drivebase() {}
+
+//Define Left Drives Motor
+CANSparkMax leftDrive1;
+CANSparkMax leftDrive2;
+//Define Right Drives Motor   
+CANSparkMax rightDrive1;
+CANSparkMax rightDrive2;
+
+DifferentialDrive allDrive;
+
+//TODO Add Gear Shifter
+
+
+  public Drivebase() {
+
+  leftDrive1 = new CANSparkMax(Constants.DriveMotorConstants.LEFT_DRIVE1_ID, MotorType.kBrushed);
+  leftDrive2 = new CANSparkMax(Constants.DriveMotorConstants.LEFT_DRIVE2_ID, MotorType.kBrushed);
+  rightDrive1 = new CANSparkMax(Constants.DriveMotorConstants.RIGHT_DRIVE1_ID, MotorType.kBrushed);
+  rightDrive2 = new CANSparkMax(Constants.DriveMotorConstants.RIGHT_DRIVE2_ID, MotorType.kBrushed);
+
+  leftDrive1.restoreFactoryDefaults();
+  leftDrive2.restoreFactoryDefaults();
+  rightDrive1.restoreFactoryDefaults();
+  rightDrive2.restoreFactoryDefaults();
+
+  leftDrive1.setIdleMode(IdleMode.kBrake);
+  leftDrive2.setIdleMode(IdleMode.kBrake);
+  rightDrive1.setIdleMode(IdleMode.kBrake);
+  rightDrive2.setIdleMode(IdleMode.kBrake);
+
+  leftDrive2.follow(leftDrive1);
+  rightDrive2.follow(rightDrive1);
+
+  leftDrive1.setInverted(true);
+
+  allDrive = new DifferentialDrive(leftDrive1, rightDrive1);
+  allDrive.setMaxOutput(1);
+  allDrive.setExpiration(0.1);
+
+  }
+/**
+ * @param left left motor speed
+ * @param right right motor speed
+ */
+
+  public void drive(double left, double right){
+    allDrive.tankDrive(left, right);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+
   }
 }
