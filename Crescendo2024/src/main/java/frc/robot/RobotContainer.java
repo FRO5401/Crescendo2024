bf4f198d723f6478a2c2bf4a6dd1acee254e16b4
Ace Hathaway <Chathaway121@gmail.com>
2024-03-07 08:42:46 -0500

    still had merge conflicts boy oh boy I love my life

--- Crescendo2024/src/main/java/frc/robot/RobotContainer.java
+++ Crescendo2024/src/main/java/frc/robot/RobotContainer.java
@@ -4,28 +4,19 @@
 
 package frc.robot;
 
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 //WPI Imports
-// WPI imports
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+//Subsystem Imports
+import frc.robot.Subsystems.Drivebase;
+import frc.robot.Subsystems.Infeed;
+
+//Command Imports
 import frc.robot.Commands.AmpShot;
-import frc.robot.Commands.BackShooter;
 import frc.robot.Commands.ClimberMove;
 import frc.robot.Commands.Expel;
 import frc.robot.Commands.Intake;
@@ -35,25 +26,13 @@ import frc.robot.Commands.RotatePivotSafe;
 import frc.robot.Commands.RotatePivotShooter;
 import frc.robot.Commands.ShiftGear;
 import frc.robot.Commands.StopAll;
-//Command Imports
 import frc.robot.Commands.XboxMove;
-import frc.robot.Commands.Auto.AutoAmpShot;
 import frc.robot.Commands.Auto.AutoShoot;
 import frc.robot.Commands.Auto.AutoTarget;
-import frc.robot.Commands.Auto.FourPieceAuto;
-import frc.robot.Commands.Auto.JustShoot;
-import frc.robot.Commands.Auto.OnePieceAuto;
-import frc.robot.Commands.Auto.SideAuto;
-import frc.robot.Commands.Auto.Test;
-import frc.robot.Commands.Auto.ThreePieceAuto;
-import frc.robot.Commands.Auto.ThreePieceFlipped;
-import frc.robot.Commands.Auto.TwoPieceAuto;
 import frc.robot.Commands.Lights.AllianceLED;
 import frc.robot.Commands.Lights.BlueLED;
-import frc.robot.Commands.Lights.GreenLED;
-import frc.robot.Commands.Lights.PurpleLED;
 import frc.robot.Commands.Lights.RainbowLED;
-import frc.robot.Commands.Lights.YellowLED;
+
 //Subsystem Imports
 import frc.robot.Subsystems.Climber;
 import frc.robot.Subsystems.Drivebase;
@@ -62,11 +41,9 @@ import frc.robot.Subsystems.LEDSubsystem;
 import frc.robot.Subsystems.Photonvision;
 import frc.robot.Subsystems.Shooter;
 
-//Used Imports that might be used in future
-//import frc.robot.Commands.StopPivot;
-
 public class RobotContainer {
-    private final CommandXboxController operator = Controls.operator;
+
+      private final CommandXboxController operator = Controls.operator;
     private final CommandXboxController driver = Controls.driver;
 
     /* Drivebase */
@@ -100,101 +77,74 @@ public class RobotContainer {
 
  
 
-
   public RobotContainer() {
     drivebase.setDefaultCommand(xboxMove);
 
     leftClimber.setDefaultCommand(new ClimberMove(leftClimber, "Left"));
     rightClimber.setDefaultCommand(new ClimberMove(rightClimber, "Right"));
-    
-
-    drivebase.resetOdometry(new Pose2d(new Translation2d(0,0), new Rotation2d(0)));
 
     configureBindings();
-    chooseAuto();
   }
 
   private void configureBindings() {
 
+    /** Operator Controls */
     /*
      * Low Power Shot - Bottom DPAD 
      * Speaker Shot - Up DPAD
      * Left climber - left joysticks
-     * right climber - right joystick
+     * Right climber - right joystick
      * Expel - Right Trigger
      * Intake - Left Trigger
      * Rotate Pivot Air - B
-     * Rotate Pivot Grouns - Y
+     * Rotate Pivot Grounds - Y
      * Rotate Pivot Shooter - A
+     * Rotate Pivot Safe - X
      * Stop All - Left Bumber
-     */
+    */
+
+    /** Driver Controls */
+    /*
+      * Rainbow LEDs - Back Button
+      * Blue LEDs - X
+      * Alliance LEDs - Y
+      * Shift Gear - Start Button
+      * Auto Target - A
+    */
 
     /** Intake Commands */
     //If "Right Trigger" pressed/held on operator controller expel command used (removes note from infeed)
-    operator.rightTrigger().onTrue(new Expel(infeed));
+    operator.rightTrigger().whileTrue(new Expel(infeed));
     //If "Left Trigger" pressed/held on operator controller intake command used (sucks note into infeed)
-    operator.leftTrigger().onTrue(new Intake(infeed));
+    operator.leftTrigger().whileTrue(new Intake(infeed));
     //If "Left Bumper" pressed/held on operator controller stop intake command used (stops infeed)
-    operator.leftBumper().onTrue(new StopAll(infeed, shooter));
+    operator.leftBumper().whileTrue(new StopAll(infeed, shooter));
 
     /** Pivot Commands */
     //If "Y" pressed/held on operator controller rotatepivotground command used (moves intake to ground)
-    operator.y().whileTrue(new SequentialCommandGroup(new RotatePivotGround(infeed), new Intake(infeed)));
+    operator.y().whileTrue(new RotatePivotGround(infeed));
     //If "A" pressed/held on operator controller rotatepivotshooter command used (moves intake to shooter)
     operator.a().whileTrue(new RotatePivotShooter(infeed));
     //If "B" pressed/held on operator controller rotatepivotAir command used (moves intake to air)
     operator.b().whileTrue(new RotatePivotAir(infeed));
     operator.x().whileTrue(new RotatePivotSafe(infeed));
-    operator.povLeft().onTrue(new BackShooter(shooter));
 
-    //Auto Shooter Methods.
-    operator.povUp().onTrue(new ParallelCommandGroup(new AutoShoot(infeed, shooter), new BlueLED(LED)));
-    operator.povDown().onTrue(new ParallelCommandGroup(new AutoAmpShot(shooter, infeed), new BlueLED(LED)));
-    
+    operator.povUp().onTrue(new AutoShoot(infeed, shooter));
+    operator.povDown().onTrue(new AmpShot(shooter));
+
 
     driver.start().onTrue(new ShiftGear(drivebase));
-
-
-    //LED COMMANDS
+    
     driver.back().onTrue(new RainbowLED(LED));
     driver.x().onTrue(new BlueLED(LED));
-    driver.y().onTrue(new YellowLED(LED));
-    driver.b().onTrue(new PurpleLED(LED));
+    driver.y().onTrue(new AllianceLED(LED));
 
 
-    //Limit Switch t stop infeed
-    hasNote.onFalse(new SequentialCommandGroup(new ParallelCommandGroup(new StopAll(infeed, shooter), new GreenLED(LED)), new RotatePivotAir(infeed)) );
-  }
-
-  public void chooseAuto(){
-    
-
-    chooser.addOption("One Piece", new OnePieceAuto(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("Two Piece", new TwoPieceAuto(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("Three Piece", new ThreePieceAuto(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("Four Piece", new FourPieceAuto(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("Flipped Three Piece", new ThreePieceFlipped(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("One Piece Side", new SideAuto(infeed, shooter, drivebase).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-    chooser.addOption("Just Shoot (For Peddie)", new JustShoot(infeed, shooter).withInterruptBehavior(InterruptionBehavior.kCancelIncoming));
-
-    
-
-
-
-    chooser.addOption("Do Nothing", Commands.print("Oops"));
-
-
-    Shuffleboard.getTab("Autonomous").add(chooser);
+   hasNote.onTrue(Commands.parallel(new RotatePivotShooter(infeed), new StopAll(infeed, shooter)));
 
   }
 
   public Command getAutonomousCommand() {
-    return chooser.getSelected();
+    return Commands.print("No autonomous command configured");
   }
-
-  public static Drivebase getDrivebase() {
-      return drivebase;
-  }
-
-
 }
